5e52bc0ba611c1ff5d368daaadf972794473c3bc
David Eliason <davethemaker@gmail.com>
2018-09-29 14:57:04 -0700

    install mongodb and MongoClient, successfully connect to mlab mongodb

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 
 const express = require('express')
-const port = process.env.PORT || 3000
 const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
+const mongo = require('mongodb')
+const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 const app = express()
+const port = process.env.PORT || 3000
+const uri = process.env.DB_MLAB
+
 
 // MIDDLEWARE
 app.use(favicon(path.join(__dirname,'public','favicon.ico')));
@@ -17,7 +21,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/api/quotes', (req, res) => res.json([{'name' : 'David'}]))
+// API ENDPOINT
+MongoClient.connect(uri, (err,db)=>{
+  console.log('db connected')
+  app.get('/api/quotes', (req, res) => res.json([{'name' : 'David'}]))
+
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
