4a364a136526e66213cfa0161bfc75fbe2b3b2cc
David Eliason <davethemaker@gmail.com>
2018-09-29 15:26:28 -0700

    successfully use fetch to grab JSON data from express server and render them using App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+ 
+   constructor(props) {
+    super(props);
+
+    this.state = {
+    quotes: [] 
+    };
+
+  }
+
+   refreshPage(){ 
+    window.location.reload(); 
+   }
+
+  getQuotes(){
+     fetch('/api/quotes')
+      .then(res => res.json())
+      .then(quotes => this.setState({ quotes }));
+  }
+
+  componentDidMount() {
+   this.getQuotes();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      <div>
+         <h1>Quotes</h1>
+           {this.state.quotes.map(quote =>
+               <div key={quote._id}> "{quote.quote}" - {quote.name}</div>
+           )}
+        </div>
+      )}
 }
 
 export default App;
