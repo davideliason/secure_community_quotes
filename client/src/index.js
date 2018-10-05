aeda2f428972b76f9d272f3945ca098c0737fc89
David Eliason <davethemaker@gmail.com>
2018-10-05 11:22:45 -0700

    add second route to BrowserRouter so that login and signup components are rendered as needed per url

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import SignupForm from './components/SignUpForm.js';
+import LoginForm from './components/LoginForm.js';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render( <Router>
+ReactDOM.render( 
+	<Router>
       <div>
         <Route path="/signup" component={SignupForm} />
+        <Route path="/login" component={LoginForm} />
       </div>
-  </Router>, document.getElementById('root'));
+    </Router>, document.getElementById('root'));
