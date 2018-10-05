1ebccbb19c7fd66569bdd2a8d70f8c901cc2f0cd
David Eliason <davethemaker@gmail.com>
2018-10-05 11:16:07 -0700

    add react-router-dom and update CRA so that the BroswerRouter acts as parent with Route components, in this case only the /signup route is currently in place

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css'
-import Quotes from './Quotes.js';
+// import Quotes from './Quotes.js';
 import SignUpForm from './SignUpForm.js';
-import LoginForm from './LoginForm.js';
+// import LoginForm from './LoginForm.js';
 
 
 class App extends Component {
@@ -15,7 +15,6 @@ class App extends Component {
     return (
       <div className="App-intro ">
           <SignUpForm />
-          <Quotes />
       </div>
     )}
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import SignupForm from './components/SignUpForm.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( <Router>
+      <div>
+        <Route path="/signup" component={SignupForm} />
+      </div>
+  </Router>, document.getElementById('root'));
