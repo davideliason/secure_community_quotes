eb1d69e9226a3dd752ae1bb1cc6bcf922ec7c7df
David Eliason <davethemaker@gmail.com>
2018-10-06 11:51:13 -0700

    building up from scratch again to test my knowledge and skills, practice makes perfect

--- database/index.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-mongoose.connect(process.env.DB_MLAB)
-  .then(() =>  console.log('mlab db connection succesful'))
-  .catch((err) => console.error(err));
--- database/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose'),
-    passportLocalMongoose = require('passport-local-mongoose');
- 
-const UserSchema =  new mongoose.Schema({});
- 
-UserSchema.plugin(passportLocalMongoose);
- 
-module.exports = mongoose.model('User', UserSchema);
