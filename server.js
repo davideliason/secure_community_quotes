d02af47f04953d43ae2c1215c01c9aec53766136
David Eliason <davethemaker@gmail.com>
2018-10-08 10:28:49 -0700

    retrieve all docs using mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const path = require('path');
 const logger = require('morgan');
@@ -11,7 +12,6 @@ const uri = process.env.DB_MLAB;
 const mongoose = require('mongoose');
 const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
 
 
 // middleware
@@ -21,17 +21,16 @@ app.use(bodyParser.urlencoded({ extended:true }));
 // app.use(express.static(path.join(__dirname, 'client/build')));
 // app.use(express.static(path.join(__dirname, 'index.html')));
 
-MongoClient.connect(uri, { useNewUrlParser: true }, (err, database) => {
+MongoClient.connect(uri, (err, database) => {
   console.log("mongodb connected");
+
+  db = database.db('secure_community_quotes');
+
 //routes
-	app.get('/users', function(req, res, next) {
-	  res.json([{
-	  	id: 1,
-	  	username: "user1"
-	  }, {
-	  	id: 2,
-	  	username: "user2"
-    }]);
+	app.get('/api/users', function(req, res, next) {
+	 db.collection('users').find().toArray( (err,users)=>{
+	 	res.json(users);
+	 });
   });
 
 });
