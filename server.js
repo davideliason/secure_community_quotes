2cbf73a4b71cd0fe227bca49111acd39d55de78f
David Eliason <davethemaker@gmail.com>
2018-10-08 11:23:44 -0700

    connect to local mongodb via mongoose

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+// dependencies
 const express = require('express');
+const mongoose = require('mongoose');
+
+//config
 const app = express();
 const port = process.env.PORT || 5000;
 
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/users')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+//routes
 app.get('/', (req,res)=>{
 	res.send("hello world");
 });
 
+// server spin up
 app.listen(port, ()=>{
 	console.log(`port up at ${port}`);
 });
+
