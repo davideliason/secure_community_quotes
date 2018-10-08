b10c43b8e7fb1faf3f9c6a562d0c0c5959514a25
David Eliason <davethemaker@gmail.com>
2018-10-08 13:48:06 -0700

    within CRA client use fetch to utilize server-side users/ route to obtain all documents of users served as API JSON data, set to state within APP component and render. Thus, this does connect the client react client with the backend data using that api

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+ 
+   constructor(props) {
+    super(props);
+
+    this.state = {
+    users: [] 
+    };
+
+    this.getUsers = this.getUsers.bind(this);
+
+  }
+
+  getUsers(){
+     fetch('/users/')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  componentDidMount() {
+   this.getUsers();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+      <div>
+
+         <h1>Users</h1>
+           {this.state.users.map(user =>
+               <div key={user._id}> {user.email} - {user.password}</div>
+           )}
+        </div>
+      )}
 }
 
 export default App;
