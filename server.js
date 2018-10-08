3516a7fe27582a56b2f9ec8c174322bc3ce02d9b
David Eliason <davethemaker@gmail.com>
2018-10-08 10:22:04 -0700

    connect mongodb

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const uri = process.env.DB_MLAB;
 
+const mongoose = require('mongoose');
 const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
@@ -20,16 +21,18 @@ app.use(bodyParser.urlencoded({ extended:true }));
 // app.use(express.static(path.join(__dirname, 'client/build')));
 // app.use(express.static(path.join(__dirname, 'index.html')));
 
-
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, database) => {
+  console.log("mongodb connected");
 //routes
-app.get('/users', function(req, res, next) {
-  res.json([{
-  	id: 1,
-  	username: "user1"
-  }, {
-  	id: 2,
-  	username: "user2"
-  }]);
-});
+	app.get('/users', function(req, res, next) {
+	  res.json([{
+	  	id: 1,
+	  	username: "user1"
+	  }, {
+	  	id: 2,
+	  	username: "user2"
+    }]);
+  });
 
+});
 app.listen(port);
