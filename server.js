3a4d3bb061180b01a37a4aad506e86bbabf6b9b7
David Eliason <davethemaker@gmail.com>
2018-10-08 18:39:57 -0700

    add option for URL string.parser deprecation

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ require('dotenv').config();
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3001;
+const uri = process.env.DB_MLAB;
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
+mongoose.connect(uri, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error'));
+console.log("db connected");
 //routes
 router.get('/', (req,res)=>{
 	res.json({ message: 'Hello, World!' });
