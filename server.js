123c88304f41f5a312460b9124f7db4d1f4a772b
David Eliason <davethemaker@gmail.com>
2018-10-09 13:02:38 -0700

    add middleware for handling multipart form uploads for requests as well as morgan logging

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const logger = require('morgan');
 const path = require('path');
 const mongoose = require('mongoose');
 
+
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+const router = express.Router();
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
 
 app.get('/', (req,res)=>{
-	res.send('hello world');
+	res.send('hello');
 });
+
 
 app.listen(port, () => {
 	console.log(`server listening at port ${port}`);
