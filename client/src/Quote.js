2a11d19577039acd002524149717498d12de1d0e
David Eliason <davethemaker@gmail.com>
2018-10-08 17:55:41 -0700

    update styling for individual Quotes

--- client/src/Quote.js
+++ client/src/Quote.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Quote = props => (
   <div className="singleQuote">
-    <p>{props.author} </p>
-    <i>{props.children}</i>
+    <i>"{props.children}"</i>
+    <p>- {props.author} </p>
   </div>
 );
 
