e968b72dc58aebc6bc9a61e4b7ad0e5ebabc97dc
David Eliason <davethemaker@gmail.com>
2018-10-10 10:56:04 -0700

    add uuid and helmet, morgan

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path	= require('path');
-const logger = require('morgan');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const uuid = require('uuid');
 
 require('dotenv').config();
 const port = process.env.PORT || 3001;
 const app = express();
 
+// MIDDLEWARE
+app.use(morgan('dev'));
+
 app.get('/',(req,res,next)=>{
-	console.log(process.env.TEST);
+	console.log(process.env.TEST + uuid());
 	res.type('text/plain');
 	res.write('hello world');
 	res.end();
