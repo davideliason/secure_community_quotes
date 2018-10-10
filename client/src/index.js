8d724ce28f7f13faf82847c64b8b053e95bc815a
David Eliason <davethemaker@gmail.com>
2018-10-10 09:30:55 -0700

    update QuoteBox parent component which serves as wrapper

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import QuoteBox from './QuoteBox.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<QuoteBox />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
