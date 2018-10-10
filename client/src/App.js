4963be4f6028efe404f781dc84e9e0a69962015e
David Eliason <davethemaker@gmail.com>
2018-10-10 11:06:46 -0700

    update CRA App component to fetch the server API endpoint to obtain JSON data and render using map

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {quotes: []}
+
+  componentDidMount() {
+    fetch('/api/quotes')
+      .then(res => res.json())
+      .then(quotes => this.setState({ quotes }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Quotes</h1>
+        {this.state.quotes.map(quote =>
+          <div key={quote.id}>{quote.author} : {quote.text}</div>
+        )}
       </div>
     );
   }
