e8466cb3e230b8772c08c48f17d3bfb2e35cbb0b
David Eliason <davethemaker@gmail.com>
2018-10-10 12:53:37 -0700

    add body-parser and mongoose, establish a connection to mlab

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ const path	= require('path');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const uuid = require('uuid');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 const port = process.env.PORT || 3001;
+const uri = process.env.DB_MLAB;
 const app = express();
 
 // MIDDLEWARE
 app.use(morgan('dev'));
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+
+mongoose.connect(uri,{ useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+console.log('db connection established');
 
 app.get('/api/quotes',(req,res,next)=> {
 	 res.json([{
