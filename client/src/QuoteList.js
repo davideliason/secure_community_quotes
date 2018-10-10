ebee77b63a8b1d38a4dce4205199ee0bf327ecd0
David Eliason <davethemaker@gmail.com>
2018-10-10 11:54:38 -0700

    update QuoteList so that it receives data as props from parent QuoteBox component

--- client/src/QuoteList.js
+++ client/src/QuoteList.js
@@ -1,39 +1,12 @@
 import React, { Component } from 'react';
 
- 
-
 class QuoteList extends Component { 
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-   	  quotes: [{
-        id: 10,
-        author: "David",
-        text: "coffee"
-        }, {
-        id: 11,
-        author: "John",
-        text:"coffee too"
-     }]
-    };
-
-    // this.handleChange = this.handleChange.bind(this);
-
-}
-
-  // componentDidMount() {
-  //   fetch('/api/quotes')
-  //     .then(res => res.json())
-  //     .then(quotes => this.setState({ quotes }));
-  // }
 
   render() {
   	return (
   		<div>
   			 <h1>Quotes</h1>
-       		 {this.state.quotes.map(quote =>
+       		 {this.props.quotes.map(quote =>
                <div key={quote.id}>{quote.author} : {quote.text}</div>
        		 )}
         </div>
