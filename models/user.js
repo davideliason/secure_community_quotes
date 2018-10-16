55d51758260f3f268fd2b2d32a2f689945cdc6b9
David Eliason <davethemaker@gmail.com>
2018-10-15 18:15:15 -0700

    add UserSchema methods for using bcryupt to hash the password as well as a checkPassword function that will be used later for logging in

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
 // create new instance of the mongoose.schema. the schema takes an
 // object that shows the shape of your database entries.
@@ -7,5 +8,14 @@ const UserSchema = new Schema({
   username: String,
   password: String
 });
+
+UserSchema.methods = {
+  checkPassword: function (inputPassword) {
+  return bcrypt.compareSync(inputPassword, this.password)
+},
+  hashPassword: plainTextPassword => {
+  return bcrypt.hashSync(plainTextPassword, 10)
+  }
+}
 
 module.exports = mongoose.model('User', UserSchema);
